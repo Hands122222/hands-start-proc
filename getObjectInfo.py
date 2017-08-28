92e70440d4af4cb61195e62342f891fdc4adc813
Hands122222 <handssky@foxmail.com>
2017-08-28 11:04:30 +0800

    add getObjectInfo.py
    add getObjectInfo.py

---
+++ getObjectInfo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- conding:utf-8 -*-
+
+#type()
+
+print('type(123):',type(123))
+
+print('type(\'123\'):',type('123'))
+
+print('type(None)',type(None))
+
+print('type(abs)',type(abs))
+
+import types
+
+print('type(\'123\')==str:',type('123')==str)
